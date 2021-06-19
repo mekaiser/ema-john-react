36dbd68f2660874e170608d38d015eaf9cf315ce
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-06-20 00:14:00 +0600

    gitignore fixed

--- src/components/Login/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDGoFF0m8rAJ7g7PDYHqK4dp6WRRHcSNZw",
-  authDomain: "ema-john-ecommerce-web.firebaseapp.com",
-  projectId: "ema-john-ecommerce-web",
-  storageBucket: "ema-john-ecommerce-web.appspot.com",
-  messagingSenderId: "803486470757",
-  appId: "1:803486470757:web:9cfcd1c956ffccb120ac8c"
-};
-  export default firebaseConfig;
