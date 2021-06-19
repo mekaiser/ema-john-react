da1b81e85c01522df6f028990e7eac9b0236aa7c
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-06-20 00:08:19 +0600

    done

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
-import './Shipment.css';
-import { useContext } from 'react';
 import { UserContext } from '../../App';
+import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
+import './Shipment.css';
 
 const Shipment = () => {
   const { register, handleSubmit, watch, errors } = useForm();
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   const onSubmit = data => {
-      console.log('form submitted', data)
+      const savedCart = getDatabaseCart();
+      const orderedDetails = {...loggedInUser, products: savedCart, shipment: data, orderTime: new Date()};
+
+      fetch('http://localhost:5000/addOrder', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(orderedDetails)
+      })
+      .then(res => res.json())
+      .then(data => {
+        if(data){
+          processOrder();
+          alert('Your order placed successfully');
+        }
+      })
+      
     };
 
   console.log(watch("example")); // watch input value by passing the name of it
